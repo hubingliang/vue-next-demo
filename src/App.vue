053e360a4ea0e4bba436662cd339e39e586c3598
bingliang.hu <bingliang.hu@advance.ai>
2020-06-21 17:52:55 +0800

    update my vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <template>
-  <img src="./logo.png">
+  <img src="./logo.png" />
   <h1>Hello Vue 3!</h1>
-  <button @click="inc">Clicked {{ count }} times.</button>
+  <p>Count: {{ count }}</p>
+  <p>Count: {{ data.count }}</p>
+  <button @click="addCount">add</button>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { reactive, ref, computed, watchEffect, watch } from "vue";
 
 export default {
   setup() {
-    const count = ref(0)
-    const inc = () => {
-      count.value++
-    }
+    const count = ref(0);
+    const data = reactive({ count: 23 });
+    const addCount = () => {
+      count.value++;
+    };
+    const plusOne = computed(() => count.value + 1);
+
+    watchEffect(() => {
+      // 依赖追踪
+      console.log("count: ", count.value);
+    });
+    // watch(count, (count, prevCount) => {
+    //   console.log("count: ", count);
+    // });
 
     return {
       count,
-      inc
-    }
+      addCount,
+      data
+    };
   }
-}
+};
 </script>
 
 <style scoped>
